7ba02f682c8b59d6688a536bf0cadeaca3c63c19
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 13:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 function driversWithRevenueOver (drivers, revenue) {
   return drivers.filter(function (driver) {return driver.revenue > revenue;});
 }
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {return driver.name;});
+}
